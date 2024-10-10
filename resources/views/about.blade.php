182b4005a21e72a623e66d316c8a7e8b54290fe9
kseni <ksenialukianova2000@gmail.com>
2024-10-11 00:32:28 +0300

    добавии

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,220 +4,127 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Главаная страница</title>
+    <link rel="stylesheet" href="main/app.css"/>
+    <link rel="stylesheet" href="css/app.css">
 </head>
 <body>
-<style>
-      .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        user-select: none;
-      }
-
-      @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-          font-size: 3.5rem;
-        }
-      }
-
-      .b-example-divider {
-        width: 100%;
-        height: 3rem;
-        background-color: rgba(0, 0, 0, .1);
-        border: solid rgba(0, 0, 0, .15);
-        border-width: 1px 0;
-        box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);
-      }
-
-      .b-example-vr {
-        flex-shrink: 0;
-        width: 1.5rem;
-        height: 100vh;
-      }
-
-      .bi {
-        vertical-align: -.125em;
-        fill: currentColor;
-      }
-
-      .nav-scroller {
-        position: relative;
-        z-index: 2;
-        height: 2.75rem;
-        overflow-y: hidden;
-      }
-
-      .nav-scroller .nav {
-        display: flex;
-        flex-wrap: nowrap;
-        padding-bottom: 1rem;
-        margin-top: -1px;
-        overflow-x: auto;
-        text-align: center;
-        white-space: nowrap;
-        -webkit-overflow-scrolling: touch;
-      }
-
-      .btn-bd-primary {
-        --bd-violet-bg: #712cf9;
-        --bd-violet-rgb: 112.520718, 44.062154, 249.437846;
-
-        --bs-btn-font-weight: 600;
-        --bs-btn-color: var(--bs-white);
-        --bs-btn-bg: var(--bd-violet-bg);
-        --bs-btn-border-color: var(--bd-violet-bg);
-        --bs-btn-hover-color: var(--bs-white);
-        --bs-btn-hover-bg: #6528e0;
-        --bs-btn-hover-border-color: #6528e0;
-        --bs-btn-focus-shadow-rgb: var(--bd-violet-rgb);
-        --bs-btn-active-color: var(--bs-btn-hover-color);
-        --bs-btn-active-bg: #5a23c8;
-        --bs-btn-active-border-color: #5a23c8;
-      }
-
-      .bd-mode-toggle {
-        z-index: 1500;
-      }
-
-      .bd-mode-toggle .dropdown-menu .active .bi {
-        display: block !important;
-      }
-    </style>
-
-    
-    <!-- Custom styles for this template -->
-    <link href="carousel.css" rel="stylesheet">
-  </head>
-  <body>
-    <svg xmlns="http://www.w3.org/2000/svg" class="d-none">
-      <symbol id="check2" viewBox="0 0 16 16">
-        <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z"/>
-      </symbol>
-      <symbol id="circle-half" viewBox="0 0 16 16">
-        <path d="M8 15A7 7 0 1 0 8 1v14zm0 1A8 8 0 1 1 8 0a8 8 0 0 1 0 16z"/>
-      </symbol>
-      <symbol id="moon-stars-fill" viewBox="0 0 16 16">
-        <path d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278z"/>
-        <path d="M10.794 3.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387a1.734 1.734 0 0 0-1.097 1.097l-.387 1.162a.217.217 0 0 1-.412 0l-.387-1.162A1.734 1.734 0 0 0 9.31 6.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387a1.734 1.734 0 0 0 1.097-1.097l.387-1.162zM13.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.156 1.156 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.156 1.156 0 0 0-.732-.732l-.774-.258a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732L13.863.1z"/>
-      </symbol>
-      <symbol id="sun-fill" viewBox="0 0 16 16">
-        <path d="M8 12a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z"/>
-      </symbol>
-    </svg>
-
-    <div class="dropdown position-fixed bottom-0 end-0 mb-3 me-3 bd-mode-toggle">
-      <button class="btn btn-bd-primary py-2 dropdown-toggle d-flex align-items-center"
-              id="bd-theme"
-              type="button"
-              aria-expanded="false"
-              data-bs-toggle="dropdown"
-              aria-label="Toggle theme (auto)">
-        <svg class="bi my-1 theme-icon-active" width="1em" height="1em"><use href="#circle-half"></use></svg>
-        <span class="visually-hidden" id="bd-theme-text">Toggle theme</span>
-      </button>
-      <ul class="dropdown-menu dropdown-menu-end shadow" aria-labelledby="bd-theme-text">
-        <li>
-          <button type="button" class="dropdown-item d-flex align-items-center" data-bs-theme-value="light" aria-pressed="false">
-            <svg class="bi me-2 opacity-50" width="1em" height="1em"><use href="#sun-fill"></use></svg>
-            Light
-            <svg class="bi ms-auto d-none" width="1em" height="1em"><use href="#check2"></use></svg>
-          </button>
-        </li>
-        <li>
-          <button type="button" class="dropdown-item d-flex align-items-center" data-bs-theme-value="dark" aria-pressed="false">
-            <svg class="bi me-2 opacity-50" width="1em" height="1em"><use href="#moon-stars-fill"></use></svg>
-            Dark
-            <svg class="bi ms-auto d-none" width="1em" height="1em"><use href="#check2"></use></svg>
-          </button>
-        </li>
-        <li>
-          <button type="button" class="dropdown-item d-flex align-items-center active" data-bs-theme-value="auto" aria-pressed="true">
-            <svg class="bi me-2 opacity-50" width="1em" height="1em"><use href="#circle-half"></use></svg>
-            Auto
-            <svg class="bi ms-auto d-none" width="1em" height="1em"><use href="#check2"></use></svg>
-          </button>
-        </li>
-      </ul>
+  <div data-collapse="medium" data-animation="default" data-duration="400" data-easing="ease" role="banner" class="navigation-bar w-nav">
+    <div class="w-container">
+      <a href="/" aria-current="page" class="brand-link w-nav-brand w--current">
+       <h1 class="brand-text">drivego</h1>
+      </a>
+      <nav role="navigation" class="navigation-menu w-nav-menu">
+        <a href="/" aria-current="page" class="navigation-link w-nav-link w--current">Главная</a>
+        <a href="/contact" class="navigation-link w-nav-link">Услуги</a>
+        <a href="/contact" class="navigation-link w-nav-link">Автомобили</a>
+        <a href="/contact" class="navigation-link w-nav-link">Бронирование</a>
+        <a href="/contact" class="navigation-link w-nav-link">Контакты</a>
+       </nav>
+       <div class="hamburger-button w-nav-button">
+         <div class="w-icon-nav-menu"></div>
+       </div>
     </div>
-
-    
-<header data-bs-theme="dark">
-  <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">Carousel</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarCollapse">
-        <ul class="navbar-nav me-auto mb-2 mb-md-0">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-          </li>
-        </ul>
-        <form class="d-flex" role="search">
-          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success" type="submit">Search</button>
-        </form>
-      </div>
+</div>
+<div class="hero-section centered">
+  <div data-w-id="e464d218-f801-55d1-1f50-7da00b5bfb8f" style="opacity:0" class="w-container">
+    <h1 data-ix="fade-in-bottom-page-loads" class="hero-heading">Найдите лучший автомобиль</h1>
+    <div data-ix="fade-in-bottom-page-loads" class="hero-subheading">Ваши путешествия начинаются здесь</div>
+    <div data-ix="fade-in-bottom-page-loads">
+      <a href="#" class="button">Вход в личный кабинет</a>
     </div>
-  </nav>
-</header>
-
-<main>
-
-  <div id="myCarousel" class="carousel slide mb-6" data-bs-ride="carousel">
-    <div class="carousel-indicators">
-      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
-      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
-    </div>
-    <div class="carousel-inner">
-      <div class="carousel-item active">
-        <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="var(--bs-secondary-color)"/></svg>
-        <div class="container">
-          <div class="carousel-caption text-start">
-            <h1>Example headline.</h1>
-            <p class="opacity-75">Some representative placeholder content for the first slide of the carousel.</p>
-            <p><a class="btn btn-lg btn-primary" href="#">Sign up today</a></p>
-          </div>
-        </div>
-      </div>
-      <div class="carousel-item">
-        <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="var(--bs-secondary-color)"/></svg>
-        <div class="container">
-          <div class="carousel-caption">
-            <h1>Another example headline.</h1>
-            <p>Some representative placeholder content for the second slide of the carousel.</p>
-            <p><a class="btn btn-lg btn-primary" href="#">Learn more</a></p>
-          </div>
-        </div>
-      </div>
-      <div class="carousel-item">
-        <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="var(--bs-secondary-color)"/></svg>
-        <div class="container">
-          <div class="carousel-caption text-end">
-            <h1>One more for good measure.</h1>
-            <p>Some representative placeholder content for the third slide of this carousel.</p>
-            <p><a class="btn btn-lg btn-primary" href="#">Browse gallery</a></p>
-          </div>
-        </div>
-      </div>
-    </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Next</span>
-    </button>
   </div>
+</div>
+<div class="section">
+  <div class="w-container">
+    <div class="section-title-group">
+      <h2 class="section-heading centered"><code>Какие услуги мы предлагаем нашим клиентам</code></h2>
+    </div>
+    <div class="w-row">
+      <div class="w-col w-col-4">
+        <div data-w-id="270e8437-efa3-df11-d438-de69b23e41e9" style="opacity:0" class="white-box"><img src="images/feather-15-white.svg" width="210" alt="" class="grid-image">
+           <h3>Краткосрчоная аренда</h3>
+           <p>Идеально подходит для тех, кто нуждается в автомобиле на несколько дней или недель.</p>
+        </div>
+    </div>
+    <div class="w-col w-col-4">
+     <div data-w-id="29c25774-570b-ddb2-69b5-f4ddbb194afd" style="opacity:0" class="white-box"><img src="images/feather2-17-white.svg" width="210" alt=""class="grid-image">
+         <h3>Долгосрочная аренда</h3>
+         <p>Отличный вариант для длительных путешествий или временной замены вашего автомобиля.</p>
+     </div>
+    </div>
+    <div class="w-col w-col-4">
+      <div data-w-id="49e69b8a-ef40-4d84-1f92-d2617143b8db" style="opacity:0" class="white-box"><img src="images/feather2-22-white.svg" alt="" class="grid-image">
+        <h3>Аренда автомобилей класса люкс</h3>
+        <р> Для тех, кто предпочитает передвигаться с комфортом и стилем, предлагаются автомобили премиум-класса.</p>
+    </div>
+   </div>
+  </div>
+ </div>
+</div>
+<div class="section accent">
+  <div class="w-container">
+   <div class="section-title-group">
+     <h2 class="section-heading centered white">Наши автомобили</h2>
+   </div>
+   <div class="w-row">
+     <div class="w-col w-col-6">
+       <div class="white-box transparent"><img src="images/city-scape. jpg" sizes="(max-width: 479px) 85vw, (max-width: 767px) 88vw, (max-width: 991px) 43vw, 430.0000305175781px" srcset="image/city-scape-p-500x334.jpeg 500w, images/city-scape-p-1080x721.jpeg 1080w, images/city-scape.jpg 1300w" alt="" class="fullwidth-image">
+         <h3 class="white-text">BMW</h3>
+       </div>
+    </div>
+    <div class="w-col w-col-6">
+      <div class="white-box transparent"><img src="images/photo-1416400639808-f41f0c149b09.jpg" sizes="(max-width: 479px) 85vw, (max-width: 767px) 88vw, (max-width: 991px) 43vw, 430.0000305175781px" srcset="image/photo-1416400639808-f41f0c149b09-p-1080x721.jpeg 1080w, image/photo-1416400639808-f41f0c149b09.jpg 1300w" alt="" class="fullwidth-image">
+        <h3 class="white-text">audi</h3>
+      </div>
+    </div>
+   </div>
+ </div>
+ <a href="#" class="hollow-button all-caps">Посмотреть все</а>
+</div>
+<div class="section">
+  <div class="w-layout-blockcontainer w-container"></div>
+</div>
+<div class="footer">
+  <div class="w-container">
+    <div class="w-row">
+      <div class="spc w-col w-col-4">
+        <h5>about velocity</h5>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius.</p>
+     </div>
+     <div class="spc w-col w-col-4">
+       <h5>useful links</h5>
+       <a href="#" class="footer-link">Phasellus gravida semper nisi</a>
+       <a href="#" class="footer-link">Suspendisse nisl elit</a>
+       <a href="#" class="footer-link">Dellentesque habitant morbi</a>
+       <a href="#" class="footer-link">Etiam sollicitudin ipsum</a>
+    </div>
+    <div class="w-col w-col-4">
+      <h5>social</h5>
+      <div class="footer-link-wrapper w-clearfix"><img src="images/social-18.svg" width="20" alt="" class="info-icon">
+        <a href="#" class="footer-link with-icon">Twitter</a>
+    </div>
+    <div class="footer-link-wrapper w-clearfix"><img src="images/social-03.svg" width="20" alt="" class="info-icon">
+      <a href="#" class="footer-link with-icon">Facebook</a>
+    </div>
+    <div class="footer-link-wrapper w-clearfix"><img src="images/social-11.svg" width="20" alt="" class="info-icon">
+      <a href="#" class="footer-link with-icon">Pinterest</a>
+    </div>
+    <div class="footer-link-wrapper w-clearfix"><img src="images/social-06.svg" width="20" alt="" class="info-icon">
+      <a href="#" class="footer-link with-icon">Google</a>
+    </div>
+    <div class="footer-link-wrapper w-clearfix"><img src="images/social-12.svg" width="20" alt="" class="info-icon">
+      <a href="#" class="footer-link with-icon">Webflow</a>
+    </div>
+   </div>
+  </div>
+ </div>
+</div>
+<div class="footer center">
+<div class="w-container">
+   <div class="footer-text">Copyright Made in Webflow.</div>
+  </div>
+</div>
+<script src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=66fb1637bac32b9912b0550f" type="text/javascript" integrity="sha256-9/aliU8dGd2tb60SsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+<script src="js/webflow.js" type="text/javascript"></script>
 </body> 
 </html>
