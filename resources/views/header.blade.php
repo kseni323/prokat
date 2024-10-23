19aa55ea1e63e4389683a7c181371fbcd36a8580
kseni <ksenialukianova2000@gmail.com>
2024-10-24 02:33:15 +0300

    first commit

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -44,6 +44,9 @@
                     <li class="nav-item">
                         <a class="nav-link" href="./#contact-us">Контакты</a>
                     </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./">Личный кабинет/a>
+                    </li>
                 </ul>
             </div>
         </div>
